967cf79c76ec5e8073f2d848666fb7643e93dd95
Iván Jiménez <908544+ivanjimenez@users.noreply.github.com>
2025-04-10 07:25:20 +0200

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,12 +4,13 @@ import './Login.css'; // Archivo de estilos CSS
 const Login = () => {
   return (
     <div className="login-container">
+      <div><img alt="" src="https://www.cloudflare.com/resources/images/slt3lc6tev37/1wXlf7IjxWvr2ivJyzC9iv/4acfec55339c30d2c01eee7d80c9a183/caching-static-content.svg" /></div>
       <div className="login-card">
         <h2 className="login-title">Iniciar Sesión</h2>
         <form className="login-form">
           <div className="form-group">
             <label htmlFor="email">Correo Electrónico</label>
-            <img alt="" src="https://www.cloudflare.com/resources/images/slt3lc6tev37/1wXlf7IjxWvr2ivJyzC9iv/4acfec55339c30d2c01eee7d80c9a183/caching-static-content.svg" />
+            
             <input type="email" id="email" placeholder="Introduce tu correo" required />
           </div>
           <div className="form-group">
